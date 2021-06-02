988f8ead3a6599016c09e9ee7262d9d0c741469f
lfcybox <lfcybox@gmail.com>
2021-06-01 21:57:05 -0300

    corner_test.py seems to be working as expected

--- corner_test.py
+++ corner_test.py
@@ -1,34 +1,44 @@
 """
-VDS and VGS sweep of NMOS from Sky130.
-Running over corners
+Simple testbench to run corners.
+The netlist placeholde SIM_BJTCORNER is modified
+on this script to run different corners
 """
 
 import ngspyce
 import numpy as np
 from matplotlib import pyplot as plt
-import time
 
-print('Sim started on: ' + time.ctime())
-# Load netlist
-tic = time.time()
-ngspyce.source('corner_test.spice')
-toc = time.time()
-print(f'ngspice netlist time: {int(toc-tic)} seconds')
-# Simulate
-tic = time.time()
-ngspyce.cmd('tran 10u 10m'.lower())
-toc = time.time()
-print(f'ngspice sim time: {int(toc-tic)} seconds')
+netlist_file = 'corner_test.spice'
+corners = ['typ','betalow']
 
-# Read results
-time, vcc_v, in_v, out_v, in_i, vcc_i = map(ngspyce.vector, ['time','vcc','in','out','vin#branch','vcc#branch'])
 
-# And plot them
-plt.plot(time*1e3, in_v, label='input')
-plt.plot(time*1e3, out_v, label='output')
+for corner in corners:
+	# Read and modify netlist
+	with open(netlist_file,'rt') as netlist:
+		ngspyce.circ(netlist.read().replace('SIM_BJTCORNER',corner))
 
-plt.title('BJT amp input and output')
-plt.xlabel('Time [ms]')
-plt.ylabel('Voltage [V]')
-plt.legend()
+	# Simulate
+	ngspyce.cmd('tran 10u 10m'.lower())
+
+	# Read results
+	time, vcc_v, in_v, out_v, in_i, vcc_i = map(ngspyce.vector, ['time','vcc','in','out','vin#branch','vcc#branch'])
+
+	# And plot them
+	plt.figure(corner)
+	plt.plot(time*1e3, in_v, label='input')
+	plt.plot(time*1e3, out_v, label='output')
+
+	plt.title('BJT amp input and output -' + corner)
+	plt.xlabel('Time [ms]')
+	plt.ylabel('Voltage [V]')
+	plt.legend()
+
+	plt.figure('combined')
+	plt.plot(time*1e3, in_v, label='input '+corner)
+	plt.plot(time*1e3, out_v, label='output '+corner)
+	plt.title('BJT amp input and output combined')
+	plt.xlabel('Time [ms]')
+	plt.ylabel('Voltage [V]')
+	plt.legend()
+
 plt.show()
